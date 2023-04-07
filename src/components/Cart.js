a1b1a4a0f4af0435d2ffe6bd31675879b8182c7d
Hansraj-singh-tomar <hansrajput033@gmail.com>
2023-04-07 10:26:20 +0530

    first commit for this

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,62 +1,16 @@
-import React, { createContext, useReducer, useEffect } from "react";
-import "./cart.css";
-import { products } from "./products";
-import ContextCart from "./ContextCart";
-import { reducer } from "./reducer";
-
-export const CartContext = createContext();
-
-const initialState = {
-  item: products,
-  totalAmount: 0,
-  totalItem: 0,
-};
+import React from 'react'
+import './cart.css'
 
 const Cart = () => {
-  // const [item, setItem] = useState(products);
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  // to delete the indv. elements from an Item Cart
-  const removeItem = (id) => {
-    return dispatch({
-      type: "REMOVE_ITEM",
-      payload: id,
-    });
-  };
-
-  // clear the cart
-  const clearCart = () => {
-    return dispatch({ type: "CLEAR_CART" });
-  };
-
-  // increment the item
-  const increment = (id) => {
-    return dispatch({
-      type: "INCREMENT",
-      payload: id,
-    });
-  };
-
-  // decrement the item
-  const decrement = (id) => {
-    return dispatch({
-      type: "DECREMENT",
-      payload: id,
-    });
-  };
-
-  // we will use the useEffect to update the data
-  useEffect(() => {
-    dispatch({ type: "GET_TOTAL" });
-    // console.log("Awesome");
-  }, [state.item]);
-
   return (
-    <CartContext.Provider
-      value={{ ...state, removeItem, clearCart, increment, decrement }}>
-      <ContextCart />
-    </CartContext.Provider>
-  );
-};
+    <>
+      <header>
+        <div className='continue-shopping'>
+          <img src="./images/arrow.png" alt="" srcset="" />
+        </div>
+      </header>
+    </>
+  )
+}
 
-export default Cart;
+export default Cart
